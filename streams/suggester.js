9c8b7e98b7e908c919bf5f51110ba51e2de3cfae
Peter Johnson <wiz@wiz.co.nz>
2014-07-29 20:10:27 +0100

    refactor to allow custom suggesters to be built

--- streams/suggester.js
+++ streams/suggester.js
@@ -1,7 +1,11 @@
  
 var through = require('through2');
+var generator = require('../lib/generators');
 
-function suggester(){
+function suggester( override ){
+
+  // allow overriding of generators
+  if( 'object' !== override ){ override = {}; }
 
   var stream = through.obj( function( record, enc, done ) {
 
@@ -13,9 +17,9 @@ function suggester(){
 
     // build suggest input/payload/output
     record.suggest = {
-      input:   suggester.generateInput( record ),
-      payload: suggester.generatePayload( record ),
-      output:  suggester.generateOutput( record )
+      input:   ( override.input   || generator.input   )( record ),
+      payload: ( override.payload || generator.payload )( record ),
+      output:  ( override.output  || generator.output  )( record )
     };
 
     this.push( record, enc );
@@ -27,66 +31,6 @@ function suggester(){
   stream.on( 'error', console.error.bind( console, __filename ) );
 
   return stream;
-}
-
-var prefixes = [ 'the' ];
-
-suggester.generateInput = function( record ){
-
-  // first should be the default name
-  var input = [
-    record.name.default.toLowerCase()
-  ];
-
-  // then add alternative names
-  for( var attr in record.name ){
-    var name = record.name[ attr ].toLowerCase();
-    input.push( name );
-  }
-
-  // allow users to exclude certain common prefixes
-  // we search for names beginning with known prefixes
-  // and followed by a space.
-  // If one is found then we add a copy of that name
-  // sans the prefix to the list of inputs
-  for( var x=0; x<input.length; x++ ){
-    var i = input[x];
-    for( var y=0; y<prefixes.length; y++ ){
-      var prefix = prefixes[y];
-      if( i.substr( 0, prefix.length+1 ) == prefix+' ' ){
-        input.push( i.substr( prefix.length+1 ) );
-      }
-    }
-  }
-
-  // de-dupe inputs
-  return input.filter( function( i, pos ) {
-    return input.indexOf( i ) == pos;
-  });
-}
-
-suggester.generatePayload = function( record ){
-  var payload = {
-    id: record.type + '/' + record.id,
-    geo: record.center_point.lon + ',' + record.center_point.lat
-  };
-  return payload;
-}
-
-suggester.generateOutput = function( record ){
-  var adminParts = [];
-
-  if( record.admin2 && record.admin2.length ){
-    adminParts.push( record.admin2 );
-  }
-  else if( record.admin1 && record.admin1.length ){
-    adminParts.push( record.admin1 );
-  }
-  if( record.admin0 && record.admin0.length ){
-    adminParts.push( record.admin0 );
-  }
-
-  return [ record.name.default ].concat( adminParts ).join(', ').trim();
 }
 
 module.exports = suggester;
