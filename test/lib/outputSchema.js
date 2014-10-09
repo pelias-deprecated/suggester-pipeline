ac91321c74d4118e06b455a1bd335729fd58ebb0
Harish Krishna <harish.krsn@gmail.com>
2014-10-09 15:19:03 -0400

    using alpha3 instead of admin0 as the key. changing outputSchema.js to outputSchema.json. Adding alpha3.json with all country codes for testing/validating purposes (http://en.wikipedia.org/wiki/ISO_3166-1_alpha-3). Modifying tests.

--- test/lib/outputSchema.js
+++ test/lib/outputSchema.js
@@ -1,75 +1,45 @@
 
-var schemas = require('../../lib/outputSchema');
+var schemas = require('../../lib/outputSchema.json');
+var alpha3  = require('../../lib/alpha3.json');
+
 module.exports.tests = {};
 
 module.exports.tests.interface = function(test, common) {
   test('interface', function(t) {
     t.equal(typeof schemas, 'object', 'valid object');
+    t.equal(schemas.hasOwnProperty('default'), true, 'has default defined');
     t.end();
   });
 };
 
-module.exports.tests.default = function(test, common) {
+module.exports.tests.valid = function(test, common) {
+  var valid_keys = ['local_admin', 'locality', 'neighborhood', 'admin2', 'admin1_abbr', 'admin1', 'admin0'];
   var default_schema = {
     local: ['local_admin', 'locality', 'neighborhood', 'admin2'],
     regional: ['admin1_abbr', 'admin1', 'admin0']
   };
-  test('schema', function(t) {
-    var schema = schemas.default;
-    t.deepEqual(schema, default_schema, 'default');
-    t.end();
-  });
-};
 
-module.exports.tests.admin0 = function(test, common) {
-  var isValid = function(keys) {
-    test('valid object', function(t) {
-      t.equal(typeof schemas[keys], 'object',  keys);
-      t.end();
-    });
-  }
-  var isNotEmpty = function(keys) {
-    test('not an empty object', function(t) {
-      t.notEqual(Object.getOwnPropertyNames(schemas[keys]).length, 0,  keys);
+  var isValid = function(keys, schema) {
+    test('valid key/object (' + keys + ')' , function(t) {
+      if (keys=="default") { 
+        t.deepEqual(schema, default_schema, 'valid default schema');
+      } else {
+        t.equal(alpha3.hasOwnProperty(keys), true, 'valid key');  
+      }
+      t.equal(typeof schema, 'object', 'valid object');
+      t.notEqual(Object.getOwnPropertyNames(schema).length, 0,  'object not empty');
+      for (levels in schema) {
+        t.equal(Object.prototype.toString.call(schema[levels]), '[object Array]', levels+' is an array');
+        for (var i=0;i<schema[levels].length;i++) {
+          var key = schema[levels][i];
+          t.notEqual(valid_keys.indexOf(key), -1, key + ' is valid');
+        }
+      }
       t.end();
     });
   }
   for (keys in schemas) { 
-    isValid(keys);
-    isNotEmpty(keys); 
-  }
-};
-
-module.exports.tests.levels = function(test, common) {
-  var check = function(keys, levels) {
-    test('valid array', function(t) {
-      t.equal(Object.prototype.toString.call(schemas[keys][levels]), '[object Array]', levels + ' (' + keys + ')');
-      t.end();
-    });
-  }
-  for (keys in schemas) { 
-    for (levels in schemas[keys]) { 
-      check(keys, levels);
-    } 
-  }
-};
-
-module.exports.tests.keys = function(test, common) {
-  var valid_keys = ['local_admin', 'locality', 'neighborhood', 'admin2', 'admin1_abbr', 'admin1', 'admin0'];
-  var check = function(i, keys, valid_keys) {
-    test('valid keys', function(t) {
-      var key = valid_keys.indexOf(i);
-      var valid_key = key != -1 ? valid_keys[key] : undefined;
-      t.equal(i, valid_key, i + ' (' + keys + ')');
-      t.end();
-    });
-  }
-  for (keys in schemas) { 
-    for (levels in schemas[keys]) { 
-      schemas[keys][levels].forEach(function(i){ 
-        check(i, keys, valid_keys);
-      }) 
-    } 
+    isValid(keys, schemas[keys]);
   }
 };
 
