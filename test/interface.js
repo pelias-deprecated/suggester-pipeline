5715fba85c0d798063dbea4cec7d04e4aeba4ac7
Severyn Kozak <severyn.kozak@gmail.com>
2015-01-16 16:25:26 -0500

    Revise/add tests to interface.js.
    test/interface.js
    	-Bundle some existing tests together, add new ones.
    	-Add a documentation comment to the module header.

--- test/interface.js
+++ test/interface.js
@@ -1,29 +1,32 @@
+/**
+ * Tests for `index.js`
+ */
+
 var suggester = require('../');
 
 tests = {};
 
-tests.streams = function(t) {
-  t.equal(typeof suggester.streams, 'object', 'valid object');
+tests[ 'exports an object with expected properties' ] = function(t) {
+  t.equal(typeof suggester, 'object', 'is an object');
+  t.ok('streams' in suggester, 'object', 'contains `streams`');
+  t.equal(typeof suggester.streams, 'object', '`streams` is an object');
+  t.equal(
+    typeof suggester.streams.suggester, 'function',
+    '`suggester` is a valid function'
+  );
   t.end();
 };
 
-tests.suggester = function(t) {
-  t.equal(typeof suggester.streams.suggester, 'function', 'valid function');
+tests[ 'exports a valid `pipeline`' ] = function (t) {
+  t.equal(typeof suggester.pipeline._read, 'function', 'is readable');
+  t.equal(typeof suggester.pipeline._write, 'function', 'is writeable');
   t.end();
 };
 
-
-tests.pipeline = function (t) {
-  t.equal(typeof suggester.pipeline, 'object', 'valid stream');
-  t.equal(typeof suggester.pipeline._read, 'function', 'valid readable');
-  t.equal(typeof suggester.pipeline._write, 'function', 'valid writeable');
-  t.end();
-};
-
-tests.generators = function(t) {
-  t.equal(typeof suggester.generators, 'object', 'valid object');
-  t.equal(typeof suggester.generators.input, 'function', 'input generator');
-  t.equal(typeof suggester.generators.output, 'function', 'output generator');
+tests[ '`generators` contains expected properties' ] = function(t) {
+  t.equal(typeof suggester.generators, 'object', 'is an object');
+  t.equal(typeof suggester.generators.input, 'function', 'has an `input`');
+  t.equal(typeof suggester.generators.output, 'function', 'has an `output`');
   t.end();
 };
 
