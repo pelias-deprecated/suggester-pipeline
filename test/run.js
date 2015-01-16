7a27d0eb143aba09580aca3e197e3fb4e4de2689
Severyn Kozak <severyn.kozak@gmail.com>
2015-01-16 15:29:48 -0500

    Include all test modules in run.js.
    test/run.js
    	-Include all test modules (now that they've been integrated).

--- test/run.js
+++ test/run.js
@@ -2,18 +2,20 @@ var tape = require( 'tape' );
 var util = require( 'util' );
 
 var testModulePaths = [
-  './interface.js'
-  // './suggester.js',
-  // './lib/generators.js',
-  // './lib/weightGenerator.js'
+  './interface.js',
+  './suggester.js',
+  './lib/generators.js',
+  './lib/weightGenerator.js'
 ];
 
 testModulePaths.forEach( function( modulePath ) {
   var tests = require( modulePath );
   for( var funcName in tests ){
-    var testName = util.format(
-      'module: %s, function: %s', modulePath, funcName
-    );
-    tape( testName, tests[ funcName ] );
+    if( typeof tests[ funcName ] === 'function' ){
+      var testName = util.format(
+        'module: %s, function: %s', modulePath, funcName
+      );
+      tape( testName, tests[ funcName ] );
+    }
   }
 });
