c6cd14d4d34d6725858d7a0d8a66f59d95db1ddb
Harish Krishna <harish.krsn@gmail.com>
2015-02-09 14:35:47 -0500

    moving weights back here

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var generators = require('./lib/generators');
 var suggester = require('./lib/suggester');
+var weights   = require('./lib/weights');
 
 var suggester = {
   streams: {
     suggester: suggester
   },
   generators: generators,
-  pipeline: suggester( generators )
+  pipeline: suggester( generators ),
+  weights: weights
 };
 
 module.exports = suggester;
